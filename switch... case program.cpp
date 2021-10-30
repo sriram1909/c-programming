bd5c9caa40fc824f3bd6675e4e36e5fbad42e968
sriram1909 <91781724+sriram1909@users.noreply.github.com>
2021-10-30 23:01:04 +0530

    Add files via upload

---
+++ switch... case program.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,choice;
+	printf("enter any two values : ");
+	scanf("%d %d",&a,&b);
+	printf("1.Add\n2.Sub\n3.Mul\n4.Div\n5.Mod\n");
+	printf("enter your choice (1 to 5) : ");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+			printf("the sum of %d and %d is %d",a,b,a+b);
+			break;
+		case 2:
+			printf("the sub of %d and %d is %d",a,b,a-b);
+			break;
+		case 3:
+			printf("the mul of %d and %d is %d",a,b,a*b);
+			break;
+		case 4:
+			printf("the div of %d and %d is %d",a,b,a/b);
+			break;
+		case 5:
+			printf("the mod of %d and %d is %d",a,b,a%b);
+			break;
+		default:
+			printf("input invalid....");
+	}
+}
