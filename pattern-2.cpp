65b23e3db732b59e46079a67e9131b2aef9f52de
sriram1909 <91781724+sriram1909@users.noreply.github.com>
2021-12-10 18:44:08 +0530

    Update pattern-2.cpp

--- pattern-2.cpp
+++ pattern-2.cpp
@@ -9,7 +9,7 @@ int main()
 		for(j=1;j<=(n-c);j++)
 			printf(" ");
 		for(i=1;i<=c;i++)
-			printf("%d ",i);
+			printf("%d ",i);//we can replace 'i' with 'c' to have increasing numbers triangle
 		printf("\n");
 	}
 }
