1f0a545b51b98e07ad7577ad040665a707741e53
sriram1909 <91781724+sriram1909@users.noreply.github.com>
2021-12-10 18:42:48 +0530

    Add files via upload

---
+++ pattern-2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,c,n;
+	printf("Enter a final value:");
+	scanf("%d",&n);
+	for(c=1;c<=n;c++)
+	{
+		for(j=1;j<=(n-c);j++)
+			printf(" ");
+		for(i=1;i<=c;i++)
+			printf("%d ",i);
+		printf("\n");
+	}
+}
